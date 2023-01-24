fd3edcaa1685c0694a4d52708796f668029b6545
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-01-23 19:19:08 -0600

    Got MVP Titles on Pages Took Some Notes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import { getMember } from '../api/memberData';
 import MemberCard from '../components/MemberCard';
 import { useAuth } from '../utils/context/authContext';
@@ -16,13 +17,18 @@ function Home() {
   }, []);
 
   return (
-    <div className="text-center my-4">
-      <div className="d-flex flex-wrap">
-        {members.map((mutant) => (
-          <MemberCard key={mutant.firebaseKey} memberObj={mutant} onUpdate={getAlltheMembers} />
-        ))}
+    <>
+      <Head>
+        <title>Welcome To Xavier&apos;s School For Gifted Youngsters!</title>
+      </Head>
+      <div className="text-center my-4">
+        <div className="d-flex flex-wrap">
+          {members.map((mutant) => (
+            <MemberCard key={mutant.firebaseKey} memberObj={mutant} onUpdate={getAlltheMembers} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
