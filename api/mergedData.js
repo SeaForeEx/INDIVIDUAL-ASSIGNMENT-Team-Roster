fac9437b1243272994d9894139a4d73dffea5e7d
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-01-24 19:39:49 -0600

    added team data and merged data code

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,11 +1,32 @@
-import { getSingleMember } from './memberData';
+import { getSingleMember, deleteMember } from './memberData';
+import { getTeamMembers, getSingleTeam, deleteSingleTeam } from './teamData';
 
 const viewMemberDetails = (memberFirebaseKey) => new Promise((resolve, reject) => {
   getSingleMember(memberFirebaseKey)
     .then((memberObject) => {
-      resolve({ ...memberObject });
+      getSingleTeam(memberObject.team_id)
+        .then((teamObject) => {
+          resolve({ teamObject, ...memberObject });
+        });
     }).catch((error) => reject(error));
 });
 
+const viewTeamDetails = (teamFirebaseKey) => new Promise((resolve, reject) => {
+  Promise.all([getSingleTeam(teamFirebaseKey), getTeamMembers(teamFirebaseKey)])
+    .then(([teamObject, teamMembersArray]) => {
+      resolve({ ...teamObject, members: teamMembersArray });
+    }).catch((error) => reject(error));
+});
+
+const deleteTeamMembers = (teamId) => new Promise((resolve, reject) => {
+  getTeamMembers(teamId).then((membersArray) => {
+    const deleteMemberPromises = membersArray.map((member) => deleteMember(member.firebaseKey));
+
+    Promise.all(deleteMemberPromises).then(() => {
+      deleteSingleTeam(teamId).then(resolve);
+    });
+  }).catch((error) => reject(error));
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export { viewMemberDetails };
+export { viewMemberDetails, viewTeamDetails, deleteTeamMembers };
---
+++ api/teamData.js
@@ -0,0 +1,92 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getTeams = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/teams.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const createTeam = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/teams.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getSingleTeam = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/teams/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteSingleTeam = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/teams/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateTeam = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/teams/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getTeamMembers = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members.json?orderBy="team_id"&equalTo="${firebaseKey}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+export {
+  getTeams,
+  createTeam,
+  getSingleTeam,
+  deleteSingleTeam,
+  updateTeam,
+  getTeamMembers,
+};
