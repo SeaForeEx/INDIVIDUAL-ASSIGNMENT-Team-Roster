f0e730cf59ab1dc66e80741e2a28d6d4446e73f7
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-01-27 08:53:45 -0600

    fixed the errors

--- pages/villains.js
+++ pages/villains.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
+import { Button } from 'react-bootstrap';
 import { getVillain } from '../api/villainData';
 import VillainCard from '../components/VillainCard';
 import { useAuth } from '../utils/context/authContext';
@@ -18,15 +19,22 @@ export default function ShowVillains() {
   }, []);
 
   return (
-    <>
+    <div className="viktorvaughn">
       <Head>
         <title>Meet The Villains!</title>
       </Head>
+      <h1>Welcome to the Hellfire Club!</h1>
+      <Button variant="outline-warning">
+        Join The Villains
+      </Button>
+      <Button variant="outline-warning">
+        Leave The Villains
+      </Button>
       <div className="d-flex flex-wrap">
         {villains.map((villain) => (
           <VillainCard key={villain.firebaseKey} villainObj={villain} onUpdate={getAllVillains} />
         ))}
       </div>
-    </>
+    </div>
   );
 }
